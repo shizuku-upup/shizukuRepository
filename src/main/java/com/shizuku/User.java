d2fee656de208e6fa0c858a57aa5f28cdb8958aa
shizuku <1120718717@qq.com>
2020-07-16 15:26:48 +0800

    添加了user的age

--- src/main/java/com/shizuku/User.java
+++ src/main/java/com/shizuku/User.java
@@ -5,6 +5,7 @@ package com.shizuku;
  * 2020/7/14
  */
 public class User {
+    private int age;
     public static void main(String[] args) {
         System.out.println("用户……");
         System.out.println("第二次修改");
