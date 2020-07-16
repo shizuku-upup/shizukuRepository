495dc9596263351e1217c6590195f36baa8710ff
shizuku <1120718717@qq.com>
2020-07-16 16:44:21 +0800

    bran1分支修改

--- src/main/java/com/shizuku/User.java
+++ src/main/java/com/shizuku/User.java
@@ -11,5 +11,6 @@ public class User {
     public static void main(String[] args) {
         System.out.println("用户……");
         System.out.println("第二次修改");
+        System.out.println("bran1修改");
     }
 }
